356f8da5261cd719170fdcde64fabbe43304709c
Terence <frade.t@belgiumcampus.ac.za>
2019-02-04 16:46:37 +0200

    Initialized frontend, created basic html for navbar and home component

--- Questionnaire-FrontendV2/src/app/app.module.ts
+++ Questionnaire-FrontendV2/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
